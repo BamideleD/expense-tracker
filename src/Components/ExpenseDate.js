fc6da3ba1f0950e05a4e3f1586ed0d7e787f9240
BamideleD <bamadboss@gmail.com>
2022-09-07 11:46:21 +0100

    Completeing the first set of components

---
+++ src/Components/ExpenseDate.js
@@ -0,0 +1,20 @@
+import "./ExpenseDate.css"
+
+
+function ExpenseDate(props){
+    const month = props.dater.toLocaleString('en-US', { month: "long"})
+    const day = props.dater.toLocaleString('en-US', { day: "2-digit"})
+    const year = props.dater.toLocaleString('en-US', { year: "numeric"})
+
+    return (
+        <div className="expense-date">
+            <div className="expense-date__month"> {month} </div>
+            <div className="expense-date__year"> {year} </div>
+            <div className="expense-date__day"> {day} </div>
+
+        </div>
+    )
+
+}
+
+export default ExpenseDate;
