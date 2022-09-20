369237efdd01600c91cfb519031926c1ef48656d
BamideleD <bamadboss@gmail.com>
2022-09-20 21:04:14 +0100

    Creating Custom Wrapper component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,11 +2,12 @@ import './Card.css';
 
 
 function Card(props){
+    const classes = 'card ' + props.className;
     return (
-        <div className = "card"> 
+        <div className = {classes}> 
         {props.children}
         </div>
-    )
+    ) 
 }
 
 export default Card;
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -13,7 +13,7 @@ function ExpenseItem(props) {
 
     return (
     <Card className="expense-item">
-        <div><ExpenseDate dater = {props.date} /></div>
+        <ExpenseDate dater = {props.date} />
         <div className="expense-item__description">
             <h2> {props.title} </h2>
             <div className="expense-item__price">${props.amount} </div>
