8ae9a7c50436efe47cfb0cc3424ca5396c241526
BamideleD <bamadboss@gmail.com>
2022-10-13 15:06:16 +0100

    Giving New Expense conditional statement

--- src/Components/NewExpenses/NewExpenses.js
+++ src/Components/NewExpenses/NewExpenses.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import './NewExpenses.css';
 import ExpenseForm from './ExpenseForm';
 
 
 const NewExpenses = (props) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+
     const SaveExpenseData = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString() 
         }
         props.onAddExpense(expenseData);
-    }
+        setIsEditing(false);
+    };
 
+    const startEditingHandler = () => {
+        setIsEditing(true);
+      };
+    
+    const stopEditingHandler = () => {
+    setIsEditing(false);
+    };
+    
     
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData = {SaveExpenseData} />
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
+            {isEditing && (           
+                <ExpenseForm 
+                    onSaveExpenseData = {SaveExpenseData}
+                    onCancel={stopEditingHandler} 
+                />
+            )}
         </div>
     )
 }
