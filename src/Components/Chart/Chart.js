9a2ae4c21e45c88c79e2748eb26cc4971ac60a43
BamideleD <bamadboss@gmail.com>
2022-10-17 09:01:08 +0100

    Creating Components for a Chart Section

---
+++ src/Components/Chart/Chart.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Chart.css';
+import ChartBar from './ChartBar';
+
+
+const chart = (props) => {
+    return(
+        <div className="chart">
+            {props.dataPoints.map((dataPoint) => 
+                <ChartBar 
+                    key = {dataPoint.label} 
+                    value = {dataPoint.value} 
+                    maxValue = {null} 
+                    label = {dataPoint.label} 
+                />
+            )}
+        </div>
+    );
+};
+
+
+
+export default chart;
